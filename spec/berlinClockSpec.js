a54154dd87fe66398ddf04ff0d3e8a6a6bbc865d
elmessari <saad.elmessari@student.vinci.be>
2024-11-11 18:35:28 +0100

    Refactor: usable constant main for all tests

--- spec/berlinClockSpec.js
+++ spec/berlinClockSpec.js
@@ -1,20 +1,17 @@
 import { Main } from "../src/berlinClock.js";
 
-
 describe("SingleMinutesRow function should return the answer ", function () {
-  it("Should return 'OOOO' for 0 minute", function () {
-    const main = new Main();
+  const main = new Main();
 
-    let result =main.singleMinutesRow(0);
+  it("Should return 'OOOO' for 0 minute", function () {
+    let result = main.singleMinutesRow(0);
 
     expect(result).toBe("OOOO");
   });
 
-  it("Should return 'YOOO' for 1 minute",function() {
-    const main =new Main();
-
-    let result=main.singleMinutesRow(1);
+  it("Should return 'YOOO' for 1 minute", function () {
+    let result = main.singleMinutesRow(1);
 
     expect(result).toBe("YOOO");
-  })
+  });
 });
