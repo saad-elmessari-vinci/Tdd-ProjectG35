3048532d255ebb28832d849c29c8c6d461843542
elmessari <saad.elmessari@student.vinci.be>
2024-11-11 18:45:56 +0100

    refactor: changed variables to constants

--- spec/berlinClockSpec.js
+++ spec/berlinClockSpec.js
@@ -4,25 +4,25 @@ describe("SingleMinutesRow function should return the answer ", function () {
   const main = new Main();
 
   it("Should return 'OOOO' for 0 minute", function () {
-    let result = main.singleMinutesRow(0);
+    const result = main.singleMinutesRow(0);
 
     expect(result).toBe("OOOO");
   });
 
   it("Should return 'YOOO' for 1 minute", function () {
-    let result = main.singleMinutesRow(1);
+    const result = main.singleMinutesRow(1);
 
     expect(result).toBe("YOOO");
   });
 
   it("Should return 'YYOO' for 2 minutes", function () {
-    let result = main.singleMinutesRow(2);
+    const result = main.singleMinutesRow(2);
 
     expect(result).toBe("YYOO");
   });
 
   it("Should return 'YYYO' for 3 minutes",function() {
-    let result =main.singleMinutesRow(3);
+    const result =main.singleMinutesRow(3);
 
     expect(result).toBe("YYYO");
   })
